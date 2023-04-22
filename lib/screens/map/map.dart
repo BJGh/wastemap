89302c45d250fa088524f83356003c5d29fbb592
Burin-Zhargal <papaev.b.n@gmail.com>
2023-04-22 13:04:38 +0300

    geolocation implemented

--- lib/screens/map/map.dart
+++ lib/screens/map/map.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:calory_calc/models/food_track_task.dart';
@@ -14,42 +16,55 @@ import 'package:calory_calc/utils/theme_colors.dart';
 import 'package:calory_calc/utils/constants.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
+import 'package:geolocator/geolocator.dart';
 
 class MapScreen extends StatefulWidget {
   @override
-  _MapScreen createState() => _MapScreen();
+  _MapScreenState createState() => _MapScreenState();
 }
 
-class _MapScreen extends State<MapScreen> {
+class _MapScreenState extends State<MapScreen> {
+  Position? _currentPosition;
+  @override
+  void initState() {
+    super.initState();
+    _getCurrentLocation();
+  }
+
+  void _getCurrentLocation() async {
+    Position position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high);
+    setState(() {
+      _currentPosition = position;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("openstreetmap Flutter"),
+        title: Text('openstreetmap Flutter'),
       ),
       body: Container(
         child: FlutterMap(
-          options:
-              MapOptions(center: LatLng(-12.069783, -77.034057), zoom: 13.0),
-          children: [
-            TileLayer(
-                urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png'),
-            MarkerLayer(markers: [
-              Marker(
-                  width: 30.0,
-                  height: 30.0,
-                  point: LatLng(-12.069783, -77.034057),
-                  builder: (ctx) => Container(
-                          child: Container(
-                        child: Icon(
-                          Icons.location_on,
-                          color: Colors.blueAccent,
-                          size: 40,
-                        ),
-                      )))
-            ])
-          ],
-        ),
+            options:
+                MapOptions(center: LatLng(-12.069783, -77.034057), zoom: 13.0),
+            children: <Widget>[
+              TileLayer(
+                urlTemplate:
+                    'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+                subdomains: ['a', 'b', 'c'],
+              ),
+              if (_currentPosition != null)
+                MarkerLayer(markers: [
+                  Marker(
+                    point: LatLng(_currentPosition?.latitude ?? 0,
+                        _currentPosition?.longitude ?? 0),
+                    builder: (ctx) =>
+                        Icon(Icons.location_on, color: Colors.red),
+                  ),
+                ]),
+            ]),
       ),
     );
   }
